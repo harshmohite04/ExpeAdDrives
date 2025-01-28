12d9eded270a7015a2e2f5ae9f710c017470cbf1
harshmohite4 <mohiteharsh639@gmail.com>
2025-01-28 13:47:11 +0530

    updated

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import logo from "../assets/logo1.jpg";
 function loginPage() {
   return (
-    <div className="bg-black text-white justify-items-center">
+    <div className="bg-black text-white justify-items-center h-screen content-center ">
       <div
         className="text-6xl font-bold
 "
@@ -10,12 +10,13 @@ function loginPage() {
         WELCOME
       </div>
       <img src={logo} className="rounded-xl w-1/3" alt="" />
-      <div className="flex flex-row py-5">
-        <div className="rounded-lg px-5 py-1 text-black" style={{ background: "#B3B3B3" }}>
+      <div className="flex flex-row py-5 ">
+        <div className="rounded-lg px-5 py-1 text-black mr-4" style={{ background: "#B3B3B3" }}>
           Login As Client
         </div>
         <div className="rounded-lg px-5 py-1 text-black" style={{ background: "#B3B3B3"}}>Login As Transport</div>
       </div>
+        <div className="rounded-lg px-5 py-1 text-black" style={{ background: "#B3B3B3"}}>Sign Up</div>
     </div>
   );
 }
